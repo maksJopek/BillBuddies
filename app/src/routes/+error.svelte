71e3281165f91da42b3876f55f9b22e5cd47d45a
Maks Jopek <maks@jopek.eu>
2025-10-31 19:06:41 +0100

    Skeleton mobile app

---
+++ app/src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<main>
+	<h1>Wystąpił błąd</h1>
+	<h3>{page.status}</h3>
+	<h2>{page.error!.message}</h2>
+</main>
+
+<style>
+	main {
+		width: 95%;
+		margin: 2rem auto;
+		text-align: center;
+	}
+	h3 {
+		margin-block: 0.5rem 1.5rem;
+		color: var(--gray);
+	}
+</style>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,19 +1,140 @@
+<script lang="ts">
+	import Input from '$lib/components/Input.svelte';
+	import Modal from '$lib/components/Modal.svelte';
+	import { addRoom, appData, deleteRoom, editRoom, type Room } from '$lib/states/data.svelte';
+	import { settings } from '$lib/states/settings.svelte';
+	import { goto } from '$app/navigation';
+
+	let modalCreateShown = $state(false);
+	let roomName = $state('');
+	let modalEditShown = $state(false);
+	let editedRoom = $state<Room>();
+	let editedRoomName = $state('');
+
+	const allBalanace = $derived(appData.rooms.reduce((sum, room) => sum + room.balance, 0));
+
+	async function saveRoom() {
+		await addRoom({
+			name: roomName,
+			userNames: {
+				[settings.user.id]: settings.user.name
+			},
+			balance: 0,
+			expenses: []
+		});
+		modalCreateShown = false;
+	}
+
+	function showEditModal(e: Event, room: Room) {
+		e.preventDefault();
+		editedRoom = room;
+		editedRoomName = room.name;
+		modalEditShown = true;
+	}
+</script>
+
 <main>
-  <h1>BillBuddies</h1>
+	<div class="top">
+		<h4>
+			Podsumowanie:
+			<span class:pos={allBalanace > 0} class:neg={allBalanace < 0}>
+				{allBalanace.toFixed(2)}zł
+			</span>
+		</h4>
+		<button onclick={() => (modalCreateShown = true)}>+ New Room</button>
+	</div>
+	<div class="rooms">
+		{#each appData.rooms ?? [] as room}
+			<button
+				onclick={() => goto(`/room/${room.id}`)}
+				oncontextmenu={(e) => showEditModal(e, room)}
+				class="room"
+			>
+				{room.name}
+				<span class:pos={room.balance > 0} class:neg={room.balance < 0}>
+					{room.balance > 0 ? '+' : ''}{room.balance.toFixed(2)}zł
+				</span>
+			</button>
+		{/each}
+	</div>
 </main>
 
-<style>
-  main {
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
+<Modal bind:open={modalCreateShown} title="Stwórz nowy pokój" onsave={saveRoom}>
+	<Input label="Nazwa pokoju" type="text" bind:value={roomName} placeholder="Berlin" />
+</Modal>
 
-  h1 {
-    font-size: 3rem;
-    font-weight: 700;
-    letter-spacing: 0.5px;
-  }
+<Modal
+	bind:open={modalEditShown}
+	title={'Edytuj pokój' + roomName}
+	saveText="Zapisz"
+	cancelText="Usuń"
+	oncancel={() => deleteRoom(editedRoom!)}
+	onsave={() => editRoom({ ...editedRoom!, name: editedRoomName })}
+	--cancel-btn-bg="var(--red)"
+>
+	<Input label="Nazwa pokoju" type="text" bind:value={editedRoomName} />
+</Modal>
+
+<style>
+	main {
+		display: grid;
+		overflow-y: auto;
+		gap: 0.5rem;
+		max-width: 500px;
+		max-height: 100%;
+	}
+
+	.top {
+		padding: 1rem;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		gap: 1rem;
+		border-bottom: 1px solid #1f1f1f;
+	}
+
+	.top button {
+		padding: 0.5rem;
+		border-radius: 0.5rem;
+		background: #4f46e5;
+		color: white;
+		font-size: 1.25em;
+	}
+
+	.top button:hover {
+		background: #1f1f1f;
+		color: #e5e5e5;
+		background: #4338ca;
+	}
+
+	.rooms {
+		display: flex;
+		flex-direction: column;
+		overflow-y: auto;
+		gap: 1rem;
+	}
+
+	.room {
+		background-color: var(--bg-dark);
+		border: 1px solid var(--border);
+		margin-inline: 1rem;
+		border-radius: 0.5rem;
+		padding: 1rem;
+		display: flex;
+		justify-content: space-between;
+		text-decoration: none;
+		cursor: pointer;
+		font-size: 1em;
+	}
+
+	.room:hover {
+		border-color: var(--primary);
+	}
+
+	.pos {
+		color: var(--green);
+	}
+	.neg {
+		color: var(--red);
+	}
 </style>
